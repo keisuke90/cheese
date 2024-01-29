9c8b1e3260698b5c2197ca7d7594da6441f71dc9
KeisukeKuwahara <kuwa.90k@gmail.com>
2024-01-30 00:09:33 +0900

    ドラッグアンドドロップ対応

--- src/components/Picture.tsx
+++ src/components/Picture.tsx
@@ -1,8 +1,9 @@
 "use client";
-import React, { useRef, useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import InputImage from "./InputImage";
 import { useGetImageUrl } from "@/hooks/useGetImageUrl";
+import { FileWithPath, useDropzone } from "react-dropzone";
 
 const IMAGE_ID = "imageId";
 
@@ -17,31 +18,40 @@ const Picture = () => {
     }
   };
 
+  const onDrop = useCallback((files: File[]) => {
+    setImageFile(files[0]);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive, acceptedFiles } =
+    useDropzone({ onDrop });
+
   const { imageUrl } = useGetImageUrl({ file: imageFile });
 
   return (
     <>
-      <label
-        htmlFor={IMAGE_ID}
-        className="border-2 border-black border-dotted w-[210px] h-[210px] flex items-center justify-center rounded-lg overflow-hidden cursor-pointer"
-      >
-        {imageUrl && imageFile ? (
-          <Image
-            src={imageUrl}
-            alt="アップロード画像"
-            width={210}
-            height={210}
-            className="w-full h-full object-cover"
+      <div {...getRootProps()} className="dropzone">
+        <label
+          htmlFor={IMAGE_ID}
+          className="border-2 border-black border-dotted w-[210px] h-[210px] flex items-center justify-center rounded-lg overflow-hidden cursor-pointer"
+        >
+          {imageUrl && imageFile ? (
+            <Image
+              src={imageUrl}
+              alt="アップロード画像"
+              width={210}
+              height={210}
+              className="w-full h-full object-cover"
+            />
+          ) : (
+            "+ 画像をアップロード"
+          )}
+          <InputImage
+            {...getInputProps()}
+            ref={fileInputRef}
+            id={IMAGE_ID}
+            onChange={handleFileChange}
           />
-        ) : (
-          "+ 画像をアップロード"
-        )}
-        <InputImage
-          ref={fileInputRef}
-          id={IMAGE_ID}
-          onChange={handleFileChange}
-        />
-      </label>
+        </label>
+      </div>
     </>
   );
 };
